616384ba489f47b4bffc281a2021d82cfa348f74
Aman Nishad <amannishad981@gmail.com>
2023-10-27 23:05:58 +0530

    Version 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.piecalculator"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.piecalculator"
@@ -19,11 +19,28 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isDebuggable = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+
+        debug {
+            isDebuggable = true
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+           )
+        }
+
+        buildFeatures {
+            viewBinding = true
         }
     }
     compileOptions {
@@ -36,12 +53,20 @@ android {
 }
 
 dependencies {
+    // MPChart Library
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    annotationProcessor("androidx.room:room-compiler:2.6.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Responsive Design
+    implementation("com.intuit.sdp:sdp-android:1.1.0")
+    // Responsive Text
+    implementation("com.intuit.ssp:ssp-android:1.1.0")
+
 }
